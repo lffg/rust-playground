2344cccc2dd6e39321ef33f3bc081e20bf0dcca1
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2022-02-11 00:13:48 -0300

    Add shared library `leet`

---
+++ common/leet/lib.rs
@@ -0,0 +1,20 @@
+#[macro_export]
+macro_rules! setup {
+    ( $main_name:ident, tests = [
+        $( ( $test_name:ident, given = ( $($input:expr),* ), expects = $output:expr ) ),* $(,)?
+    ] ) => {
+        pub struct Solution;
+
+        #[cfg(test)]
+        mod tests {
+            use super::*;
+            $(
+                #[test]
+                fn $test_name() {
+                    let output = Solution::$main_name($($input,)*);
+                    assert_eq!(output, $output);
+                }
+            )*
+        }
+    };
+}
