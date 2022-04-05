72d1ac81a04f1ea91af70d51e92c81427867d68b
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2022-04-04 22:29:18 -0300

    Add new example

---
+++ experiments/bin-alarms/src/lib.rs
@@ -0,0 +1,42 @@
+pub const AVAILABLE_ACTIONS: &[&str] = &[
+    "Ataque Magnético",
+    "Status da Válvula",
+    "Vazamento no Pulso 2",
+    "Corte de Cabo do Pulso 2",
+    "Vazamento no Pulso 1",
+    "Corte de Cabo do Pulso 1",
+];
+
+pub fn parse_alarm(hex: &str) -> Vec<&'static str> {
+    let num = u32::from_str_radix(hex, 16).unwrap();
+    AVAILABLE_ACTIONS
+        .iter()
+        .enumerate()
+        .filter_map(|(index, &action)| (num & 1 << index != 0).then(|| action))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    macro_rules! test {
+        ($name:ident, ($input:expr, $radix:expr), $out:expr) => {
+            #[test]
+            fn $name() {
+                let parsed = u32::from_str_radix($input, $radix).unwrap();
+                let hex_string = format!("{:x}", parsed);
+                let out = super::parse_alarm(&hex_string);
+                assert_eq!(out, Vec::from($out));
+            }
+        };
+    }
+
+    test!(t1, ("00000001", 2), ["Ataque Magnético"]);
+    test!(t2, ("00000010", 2), ["Status da Válvula"]);
+    test!(t3, ("00010000", 2), ["Vazamento no Pulso 1"]);
+    test!(t4, ("00010000", 2), ["Corte de Cabo do Pulso 1"]);
+    test!(
+        t5,
+        ("00100001", 2),
+        ["Ataque Magnético", "Corte de Cabo do Pulso 1"]
+    );
+}
